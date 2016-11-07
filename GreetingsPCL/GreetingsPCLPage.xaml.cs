62143589a489d5e7381c4b5ea3320cbdb1eb7e3a
utkarshSinghRathor <uutkarshsingh@gmail.com>
2016-11-07 14:40:50 +0530

    First commit and added the ContentPage file

---
+++ GreetingsPCL/GreetingsPCLPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GreetingsPCL
+{
+	public partial class GreetingsPCLPage : ContentPage
+	{
+		public GreetingsPCLPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ GreetingsPCL/GreetingsPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GreetingsPCL
+{
+	public class GreetingsPage : ContentPage
+	{
+		public GreetingsPage()
+		{
+			Label lbl = new Label();
+			lbl.Text = "Greetings from Xamarin forms";
+			this.Content = lbl;		
+		}
+	}
+}
+
